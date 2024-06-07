ec92cb65534e298c78078433f71be5b630193400
lohith1601 <lohithganapathy16@gmail.com>
2024-06-07 18:55:41 +0530

    Eventv4

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import '@/app/globals.css'
+import { Inter } from "next/font/google";
+
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+   <div className="flex items-center w-full justify-center min-h-screen bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+    {children}
+   </div>
+  );
+}
